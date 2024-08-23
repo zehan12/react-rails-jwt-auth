96e632c9678130956557acc679abb3cf9ea95e4a
zehan12 <zehan9211@gmail.com>
2024-08-23 13:40:52 +0530

    add cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,21 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    # Where your request comes from eg: your react app's IP adderss or URL
+    # dev
+    origins "http://127.0.0.1:5173"
+
+    # prod
+    # origins "https://www..vercel"
+
+    # all
+    # origins "*"
+
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
